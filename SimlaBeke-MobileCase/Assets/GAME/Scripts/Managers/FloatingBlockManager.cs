8063b3a732723b3473ca29ddaf6bf1de6f63ede8
Simla4 <simlabeken4@gmail.com>
2026-02-07 23:03:03 +0300

    uçma animasyonuna delay eklendi

--- SimlaBeke-MobileCase/Assets/GAME/Scripts/Managers/FloatingBlockManager.cs
+++ SimlaBeke-MobileCase/Assets/GAME/Scripts/Managers/FloatingBlockManager.cs
@@ -1,23 +1,45 @@
+using System;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using DG.Tweening;
 using Lean.Pool;
+using sb.eventbus;
 using UnityEngine;
 
 public class FloatingBlockManager : MonoSingleton<FloatingBlockManager>
 {
     [SerializeField] private GameObject floatingBlockPrefab;
     [SerializeField] private Canvas canvas;
-    
+
+    private EventListener<OnClickedTileEvent> onClickedTileEvent;
+    private int index = 0;
+
+    private void OnEnable()
+    {
+        onClickedTileEvent = new EventListener<OnClickedTileEvent>(ResetIndex);
+        EventBus<OnClickedTileEvent>.AddListener(onClickedTileEvent);
+    }
+
+    private void OnDisable()
+    {
+        EventBus<OnClickedTileEvent>.RemoveListener(onClickedTileEvent);
+    }
+
     public void TryToSpawnFloatingBlock(Block block)
     {
         if(!IsGoal(block)) return;
 
         var uiElement = UIManager.Instance.GetGoalUIElement(block.GetTileID());
         var spawnPosition = WorldToUISpace(block.transform.position);
-        
-        var floatingBlock = LeanPool.Spawn(floatingBlockPrefab, spawnPosition, Quaternion.identity, canvas.transform);
 
-        floatingBlock.GetComponent<BlockAnimation>().Initialize(block.GetTileData(), uiElement);
+        DOVirtual.DelayedCall(index * 0.1f, () =>
+        {
+            var floatingBlock = LeanPool.Spawn(floatingBlockPrefab, spawnPosition, Quaternion.identity, canvas.transform);
+
+            floatingBlock.GetComponent<BlockAnimation>().Initialize(block.GetTileData(), uiElement);
+        });
+        
+        index++;
     }
 
     private bool IsGoal(Block block)
@@ -32,5 +54,10 @@ public class FloatingBlockManager : MonoSingleton<FloatingBlockManager>
         Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPosition);            
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, screenPos, canvas.worldCamera, out Vector2 localPoint);
         return canvas.transform.TransformPoint(localPoint);
+    }
+
+    private void ResetIndex(OnClickedTileEvent e)
+    {
+        index = 0;
     }
 }
